daa2c7946fce505fcf65e121db81538646d9323b
JoelAngels <joelbaraka4325@gmail.com>
2021-05-19 11:30:22 +0300

    Created the App Component that renders the API CALL through the loading component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Results from "./components/Results";
+import Search from "./components/Search";
+import listLoading from "./components/Loading";
+import axios from "axios";
 
 function App() {
+  const ListLoading = listLoading(Results);
+  const [repoList, setRepoList] = useState({});
+
+  // create a function that expects a string argument, that then concatinates 3 strings into one url string.
+
+  // replace the 2nd string with search term
+  const searchHandler = (searchText) => {
+    axios.get(urlBuilder(searchText)).then((repos) => {
+      setRepoList(repos.data);
+    });
+  };
+  // make a request using the newly created url.
+  const urlBuilder = (user) => {
+    const url = `https://api.github.com/search/users?q=${user}%20in:login`;
+    return url;
+  };
+
+  // useEffect(() => {
+  //   const apiUrl = `https://api.github.com/search/users?q=foo%20in:login`;
+  //   axios.get(urlBuilder("foo")).then((repos) => {
+  //     setRepoList(repos.data);
+  //   });
+  // }, [setRepoList]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>My Repositories</h1>
+      </div>
+      <div className="repo-container">
+        <Search searchHandler={searchHandler} />
+        <ListLoading repos={repoList} />
+      </div>
     </div>
   );
 }
-
 export default App;
+
+/*
+
+https://api.github.com/search/users?q=tetris%20in:login&page=1&per_page=10
+
+*/
+
+/*
+
+https://api.github.com/search/users?q=tetris%20in:login&order=desc&page=1&per_page=10
+
+Add material ui pagination component
+Underneath the table
+
+
+Change the page number in the url string to match the page number in the material ui pagination component
+array.sort based on userName
+*/
